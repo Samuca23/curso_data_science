d9a03c78f66ca723e9f968af7d387558864a32b9
Samuel Chiodini <samuelchiodini@hotmail.com>
2023-04-03 01:13:03 +0000

    dicionario

---
+++ pyhton_basic/dicionario.py
@@ -0,0 +1,22 @@
+meu_set = {3,2,5}
+
+meu_dicionario = {'nome': 'Samuel', 'idade' : 23, 'notas': [10, 4, 8, 7]}
+
+print(meu_dicionario['nome'])
+print(meu_dicionario['idade'])
+print(meu_dicionario['notas'])
+
+
+universidades = [
+    {
+        'nome' : 'UNIDAVI',
+        'cidade' : 'Rio do Sul'
+    },
+    {
+        'nome' : 'UDESC',
+        'cidade' : 'Rio do Sul'
+    }
+]
+
+print(universidades[0]['nome'])
+
